07403df254201545374c28f5d1a0e5ca35d5ff82
Artyom Parygin <arteme.parygin@yandex.ru>
2024-02-10 23:14:37 +0300

    delete user validation

--- src/features/users/api/models/user.input.model.ts
+++ src/features/users/api/models/user.input.model.ts
@@ -6,28 +6,28 @@ import {
 } from '../../../../common/decorators/validators/user-validator.decorator';
 
 export class CreateUserModel {
-  @IsLoginExist({ message: 'Login already exist' })
-  @Matches(/^[a-zA-Z0-9_-]*$/, {
-    message: 'Invalid login pattern',
-  })
-  @Length(3, 10, { message: 'Invalid login length' })
-  @Transform(({ value }) => value?.trim())
-  @IsString()
-  @IsNotEmpty()
+  // @IsLoginExist({ message: 'Login already exist' })
+  // @Matches(/^[a-zA-Z0-9_-]*$/, {
+  //   message: 'Invalid login pattern',
+  // })
+  // @Length(3, 10, { message: 'Invalid login length' })
+  // @Transform(({ value }) => value?.trim())
+  // @IsString()
+  // @IsNotEmpty()
   login: string;
 
-  @Length(6, 20, { message: 'Invalid password length' })
-  @Transform(({ value }) => value?.trim())
-  @IsString()
-  @IsNotEmpty()
+  // @Length(6, 20, { message: 'Invalid password length' })
+  // @Transform(({ value }) => value?.trim())
+  // @IsString()
+  // @IsNotEmpty()
   password: string;
 
-  @IsEmailExist({ message: 'Email already exist' })
-  @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {
-    message: 'Invalid email pattern',
-  })
-  @Transform(({ value }) => value?.trim())
-  @IsString()
-  @IsNotEmpty()
+  // @IsEmailExist({ message: 'Email already exist' })
+  // @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {
+  //   message: 'Invalid email pattern',
+  // })
+  // @Transform(({ value }) => value?.trim())
+  // @IsString()
+  // @IsNotEmpty()
   email: string;
 }
